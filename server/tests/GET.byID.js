4cef0f710fc039dad0e10a0983bd78f8216e2173
MCFrank16 <mecfrank16@gmail.com>
2019-03-07 18:54:53 +0200

    [Chore -#164462750] add tests of delete message

--- server/tests/GET.byID.js
+++ server/tests/GET.byID.js
@@ -1,14 +1,16 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../src/server';
-import message, { Messages } from '../src/models/epicMessages';
-import uuid from 'uuid';
+import { Messages } from '../src/models/epicMessages';
+import uuid from 'chai-uuid';
+import date from 'chai-datetime';
 
 let should = chai.should;
 let expect = chai.expect;
 
 chai.use(chaiHttp);
-chai.use(require('chai-uuid'));
+chai.use(uuid);
+chai.use(date);
 
 
 
@@ -34,7 +36,7 @@ describe('GET a message by its specific ID', (done) => {
       chai.request(server)
       .get(`/api/v1/Messages/${id}`)
       .end((err,res) => {
-          res.should.have.status(200);
+          expect(res.body.status).to.eql(200);
           done();
       })
     });
@@ -43,18 +45,33 @@ describe('GET a message by its specific ID', (done) => {
       chai.request(server)
       .get('/api/v1/Messages/:id')
       .end((err,res) => {
+          
           res.body.should.be.an('object');
           done();
       });
     });
 
-    it('should also check the ID to exist and also if it has a uuid format', (done) => {
+    it('should have a full body response with 9 properties', (done) => {
        
+     
        chai.request(server)
        .get(`/api/v1/Messages/${id}`)
        
        .end((err,res) => {
-           expect(id).to.be.a.uuid('v4');
+           if(id){
+     
+            expect(res.body.data).to.have.property('id').to.be.a.uuid('v4');
+            expect(res.body.data).to.have.property('createdOn').to.be.a('string');
+            expect(res.body.data).to.have.property('subject').to.be.a('string');
+            expect(res.body.data).to.have.property('message').to.be.a('string');
+            expect(res.body.data).to.have.property('status').to.be.a('string');
+            expect(res.body.data).to.have.property('parentMessageId').to.be.a.uuid('v4');
+            expect(res.body.data).to.have.property('senderId').to.be.a.uuid('v4');
+            expect(res.body.data).to.have.property('receiverId').to.be.a.uuid('v4');
+            expect(res.body.data).to.have.property('receiverEmail').to.be.a('string');
+            
+
+           }
            done();
        })
 
