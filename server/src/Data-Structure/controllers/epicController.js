14593d84d8e4f12196f02c19e2a892679212db42
MCFrank16 <mecfrank16@gmail.com>
2019-03-11 19:54:04 +0200

    [bg -#164544686] correcting the controller to be class based

--- server/src/Data-Structure/controllers/epicController.js
+++ server/src/Data-Structure/controllers/epicController.js
@@ -1,21 +1,24 @@
 import uuid from 'uuid';
 import { epicMessages,Messages} from '../models/epicMessages';
 
-const messageData = {
+class messageData  {
     
     // get all the messages
-    getAllMessages(req,res) {
+   static getAllMessages(req,res) {
         const allMessages = epicMessages;
         res.send({
             status: 200,
             data: allMessages
         });
       
-    },
+    }
 
     // post/ create a message
-    postMessage (req,res){
-      if(!req.body.message || !req.body.subject || !req.body.receiverEmail) {
+    static postMessage (req,res){
+
+        const { message, subject, receiverEmail, status } = req.body;
+
+      if(!message || !subject || !receiverEmail) {
           return res.send({
               status: 400,
               message: 'Please fill in all the required fields'
@@ -26,13 +29,13 @@ const messageData = {
           const newPost = new Messages ({
             id: uuid.v4(),
             createdOn: Date.now().toString(),
-            subject: req.body.subject,
-            message: req.body.message,
-            status: req.body.status,
+            subject: subject,
+            message: message,
+            status: status,
             parentMessageId: uuid.v4(),
             senderId: uuid.v4(),
             receiverId: uuid.v4(),
-            receiverEmail: req.body.receiverEmail
+            receiverEmail: receiverEmail
 
           });
 
@@ -41,15 +44,15 @@ const messageData = {
               status: 201,
               data: newPost
           });
-      },
+      }
     
-      getUnreadMessages(req,res) {
+      static getUnreadMessages(req,res) {
          let unreadVal = new Messages();
          let unreadMessages={
              status: 200,
              data: []
          };
-         const unRead = epicMessages.filter( unreadVal => unreadVal.status === 'Unread');
+         const unRead = epicMessages.filter( unreadVal => unreadVal.status === 'unread');
          if(!unRead.length){
             unreadMessages={
                 status: 200,
@@ -59,15 +62,15 @@ const messageData = {
             unreadMessages.data = unRead;
          }
          return res.send(unreadMessages)
-      },
+      }
 
-      getSentMessages(req,res) {
+      static getSentMessages(req,res) {
         let sentVal = new Messages();
         let sentMessages={
             status: 200,
             data: []
         };
-        const unRead = epicMessages.filter( sentVal => sentVal.status === 'Sent');
+        const unRead = epicMessages.filter( sentVal => sentVal.status === 'sent');
         if(!unRead.length){
            sentMessages={
                status: 200,
@@ -77,9 +80,9 @@ const messageData = {
            sentMessages.data = unRead;
         }
         return res.send(sentMessages)
-     },
+     }
 
-     getByMessageId(req,res){
+     static getByMessageId(req,res){
          let byId = new Messages();
          const { id } = req.params;
          let byIdMessages = {
@@ -100,10 +103,10 @@ const messageData = {
          }
          return res.send(byIdMessages);
 
-     },
+     }
 
      
-     deleteMessageById(req,res){
+     static deleteMessageById(req,res){
         let deleteval = new Messages();
         const { id } = req.params;
         let deleteMessages = {
