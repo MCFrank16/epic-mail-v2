4aa60d003e000e06be7a9f44cb3fb60649245ff9
MCFrank16 <mecfrank16@gmail.com>
2019-03-12 13:53:11 +0200

    [bg -#164567946] removing the capital letter at the start

--- server/src/Data-Structure/routers/route.js
+++ server/src/Data-Structure/routers/route.js
@@ -9,21 +9,21 @@ router.get('/api/v1', (req, res) => {
     res.send({ message: 'Welcome to Epic Mail Service API EndPoint' });
 });
 //get all Epic Messages Endpoint
-router.get('/api/v1/Messages', epicControllers.getAllMessages);
+router.get('/api/v1/messages', epicControllers.getAllMessages);
 
 // POST/create/send a message
-router.post('/api/v1/Messages',epicControllers.postMessage);
+router.post('/api/v1/messages',epicControllers.postMessage);
 
 // Get all unread Messages
-router.get('/api/v1/Messages/Unread', epicControllers.getUnreadMessages);
+router.get('/api/v1/messages/Unread', epicControllers.getUnreadMessages);
 
 // Get all sent Messages
-router.get('/api/v1/Messages/Sent', epicControllers.getSentMessages);
+router.get('/api/v1/messages/Sent', epicControllers.getSentMessages);
 
 // Get Message by Id
-router.get('/api/v1/Messages/:id', epicControllers.getByMessageId);
+router.get('/api/v1/messages/:id', epicControllers.getByMessageId);
 
 // Delete Message by Id
-router.delete('/api/v1/Messages/:id', epicControllers.deleteMessageById);
+router.delete('/api/v1/messages/:id', epicControllers.deleteMessageById);
 
 export default router;
