7bdb2b7985b2d0aade400f58e25e2ee496b0be4c
MCFrank16 <mflohost@gmail.com>
2019-03-14 20:24:27 +0200

    update develop

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import routes from '../src/DataStructure/Routers/route'
-import routes from '../src/Data-Structure/routers/route';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
 
 const app = express();
-const swaggerDocument = YAML.load('server/src/Docs/ApiSwagger.yml');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 
 app.use('*', (req,res) => res.status(404).send({
    'Status': 404,
