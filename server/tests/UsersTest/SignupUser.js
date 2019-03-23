25b6fa78cdfe2e5acd0a275c37328b27b458320a
MCFrank16 <mecfrank16@gmail.com>
2019-03-23 13:18:51 +0200

    checking test

--- server/tests/UsersTest/SignupUser.js
+++ server/tests/UsersTest/SignupUser.js
@@ -8,38 +8,38 @@ const { should } = chai;
 chai.use(chaiHttp);
 
 describe('create a user to the database', () => {
-  // it('Should create an account', (done) => {
-  //   const user = {
-  //     firstname: 'Alicee',
-  //     lastname: 'Mujyamberee',
-  //     email: 'AliceMujyaee@gmail.com',
-  //     password: 'ROB123',
-  //     isAdmin: 'true',
-  //     Phone: '0783200000',
-  //   };
-  //   chai.request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .send(user)
-  //     .end((err, res) => {
-  //       console.log(res.body);
-  //       expect(res.body).to.be.an('Object');
-  //       expect(res.body).to.have.property('status').equal(201);
-  //       expect(res.body).to.have.property('data');
-  //       expect(res.body.data).to.be.an('Object');
-  //       expect(res.body.data).to.have.property('token');
-  //       expect(res.body.data).to.have.property('user');
-  //       expect(res.body.data.user).to.have.property('firstname', user.firstname);
-  //       expect(res.body.data.user).to.have.property('lastname', user.lastname);
-  //       expect(res.body.data.user).to.have.property('email', user.email);
-  //       expect(res.body.data.user).to.have.property('Phone', user.Phone);
-  //       expect(res.body.data.user).to.have.property('isAdmin').to.eql('true');
-  //       done();
-  //     });
-  // });
+  it('Should create an account', (done) => {
+    const user = {
+      firstname: 'Ali',
+      lastname: 'Mujya',
+      email: 'AliMujya@gmail.com',
+      password: 'ROB123',
+      isAdmin: 'true',
+      Phone: '0783200000',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        console.log(res.body);
+        expect(res.body).to.be.an('Object');
+        expect(res.body).to.have.property('status').equal(201);
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.be.an('Object');
+        expect(res.body.data).to.have.property('token');
+        expect(res.body.data).to.have.property('user');
+        expect(res.body.data.user).to.have.property('firstname', user.firstname);
+        expect(res.body.data.user).to.have.property('lastname', user.lastname);
+        expect(res.body.data.user).to.have.property('email', user.email);
+        expect(res.body.data.user).to.have.property('Phone', user.Phone);
+        expect(res.body.data.user).to.have.property('isAdmin').to.eql('true');
+        done();
+      });
+  });
 
   it('Should be able to login', (done) => {
     const login = {
-      email: 'AliceMujyaee@gmail.com',
+      email: 'AliMujya@gmail.com',
       password: 'ROB123',
     };
     chai.request(app)
